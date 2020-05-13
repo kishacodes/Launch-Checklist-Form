97f0407cf0ac1483589a4f9dcb75e5733a15a884
Kisha Codes <Kishacodes@gmail.com>
2020-05-13 13:46:51 -0400

    1. Working on validation

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Write your JavaScript code here!
-
+//validate all text entries
+let allInfo = document.querySelectorAll("input[type=text]:checked");
+if (pilotName.value === "" && formSubmit.value === "on") {
+  alert("All fields are required!");
+  event.preventDefault();
+}
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
