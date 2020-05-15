594272462c4502055b048820fe6cf95a73826044
Kisha Codes <Kishacodes@gmail.com>
2020-05-14 21:26:13 -0400

    3. Pt 1, 2, &3 done

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
 // Write your JavaScript code here!
 //validate all text entries
-
 window.addEventListener("load", function () {
   let form = document.querySelector("form");
-  form.addEventListener("submit", function () {
-    let pilotNameInput = document.querySelector("input[name=pilotName]");
-    let copilotNameInput = document.querySelector("input[name=copilotName]");
-    let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
-    let cargoMassInput = document.querySelector("input[name=cargoMass]");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let pilotNameInput = document.querySelector("input[name=pilotName]").value;
+    let copilotNameInput = document.querySelector("input[name=copilotName]")
+      .value;
+    let fuelLevelInput = document.querySelector("input[name=fuelLevel]").value;
+    let cargoMassInput = document.querySelector("input[name=cargoMass]").value;
+
+    let pilotStatus = document.getElementById("pilotStatus").value;
+    let copilotStatus = document.getElementById("copilotStatus").value;
+    let fuelStatus = document.getElementById("fuelStatus").value;
+    let cargoStatus = document.getElementById("cargoStatus").value;
     if (
-      pilotNameInput.value === "" ||
-      (copilotNameInput.value === "") | (fuelLevelInput.value === "") ||
+      pilotNameInput === "" ||
+      copilotNameInput === "" ||
+      fuelLevelInput === "" ||
       cargoMassInput === ""
     ) {
       alert("All fields required.");
-    } else if (pilotNameInput.value >= 0 || copilotNameInput.value >= 0) {
-      alert("Pilot names must be entered with letters.");
     } else if (
-      isNaN(cargoMassInput.value) === true ||
-      isNaN(fuelLevelInput.value) === true
+      !isNaN(Number(pilotNameInput)) ||
+      !isNaN(Number(copilotNameInput))
     ) {
+      alert("Pilot names must be entered with letters.");
+    } else if (isNaN(cargoMassInput) || isNaN(fuelLevelInput)) {
       alert("Fuel Level and Cargo Mass must be entered as numbers.");
-      preventDefault();
+    } else {
+      document.getElementById("faultyItems").style.visibility = "visible";
     }
   });
 });
+
+// function process() {
+//   let pilotVar = document.form.pilotName.value;
+// }
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
